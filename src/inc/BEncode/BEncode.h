12bfd56d27ce8147149a71c29c0e08c79f21f2b8
root <root@localhost.hon4.ddns.net>
2025-04-13 19:02:38 +0000

    Added support for std::uint32_t

--- src/inc/BEncode/BEncode.h
+++ src/inc/BEncode/BEncode.h
@@ -2,6 +2,7 @@
 #include <map>
 #include <any>
 #include <vector>
+#include <cstdint>
 
 //modified
 
@@ -23,6 +24,10 @@ class BEncode {
 		std::string BEncode_int_long(long i) {
 			return "i" + std::to_string(i) + "e";
 		}
+
+		std::string BEncode_int_uint32_t(std::uint32_t i) {
+			return "i" + std::to_string(i) + "e";
+		}
 		
 		std::string BEncode_dict(std::map<std::string, std::any> d) {
 			std::string ret = "d";
@@ -36,6 +41,8 @@ class BEncode {
 					ret += BEncode_list(std::any_cast<std::vector<std::any>>(x.second));
 				} else if (x.second.type() == typeid(long)) {
 					ret += BEncode_int_long(std::any_cast<long>(x.second));
+				} else if (x.second.type() == typeid(std::uint32_t)) {
+					ret += BEncode_int_long(std::any_cast<std::uint32_t>(x.second));
 				} else {
 					ret += BEncode_int(std::any_cast<int>(x.second));
 				}
@@ -54,6 +61,8 @@ class BEncode {
 					ret += BEncode_list(std::any_cast<std::vector<std::any>>(x));
 				} else if (x.type() == typeid(long)) {
 					ret += BEncode_int_long(std::any_cast<long>(x));
+				} else if (x.type() == typeid(std::uint32_t)) {
+					ret += BEncode_int_long(std::any_cast<std::uint32_t>(x));
 				} else {
 					ret += BEncode_int(std::any_cast<int>(x));
 				}
