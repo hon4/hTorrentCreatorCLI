aa58acfe761f09d20ea416ec0a7bcf043b339aab
hon4 <greek.team.blog2@gmail.com>
2025-04-19 01:57:46 +0300

    Update int types

--- src/inc/BEncode/BEncode.h
+++ src/inc/BEncode/BEncode.h
@@ -29,6 +29,10 @@ class BEncode {
 			return "i" + std::to_string(i) + "e";
 		}
 		
+		std::string BEncode_int_uint64_t(std::uint64_t i) {
+                        return "i" + std::to_string(i) + "e";
+                }
+
 		std::string BEncode_dict(std::map<std::string, std::any> d) {
 			std::string ret = "d";
 			for (const auto& x : d) {
@@ -42,7 +46,9 @@ class BEncode {
 				} else if (x.second.type() == typeid(long)) {
 					ret += BEncode_int_long(std::any_cast<long>(x.second));
 				} else if (x.second.type() == typeid(std::uint32_t)) {
-					ret += BEncode_int_long(std::any_cast<std::uint32_t>(x.second));
+					ret += BEncode_int_uint32_t(std::any_cast<std::uint32_t>(x.second));
+				} else if (x.second.type() == typeid(std::uint64_t)) {
+					ret += BEncode_int_uint64_t(std::any_cast<std::uint64_t>(x.second));
 				} else {
 					ret += BEncode_int(std::any_cast<int>(x.second));
 				}
@@ -62,7 +68,9 @@ class BEncode {
 				} else if (x.type() == typeid(long)) {
 					ret += BEncode_int_long(std::any_cast<long>(x));
 				} else if (x.type() == typeid(std::uint32_t)) {
-					ret += BEncode_int_long(std::any_cast<std::uint32_t>(x));
+					ret += BEncode_int_uint32_t(std::any_cast<std::uint32_t>(x));
+				} else if (x.type() == typeid(std::uint64_t)) {
+					ret += BEncode_int_uint64_t(std::any_cast<std::uint64_t>(x));
 				} else {
 					ret += BEncode_int(std::any_cast<int>(x));
 				}
