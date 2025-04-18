4581f7825f31a25517abba47b7de612126c4e90e
u0_a202 <u0_a202@localhost.(none)>
2025-04-19 01:08:46 +0300

    Update long to uint64_t

--- src/inc/GetFileSize.h
+++ src/inc/GetFileSize.h
@@ -1,10 +1,10 @@
 #include <iostream>
 #include <fstream>
 
-long GetFileSize(const std::string& filename) {
+uint64_t GetFileSize(const std::string& filename) {
     std::ifstream file(filename, std::ios::binary | std::ios::ate);
     if (!file) {
         return -1; // Return -1 to indicate an error opening the file
     }
-    return static_cast<long>(file.tellg());
+    return static_cast<uint64_t>(file.tellg());
 }
