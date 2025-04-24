93d0525b37f02da89edc366350fbcd53ce5bcd66
hon <38807226+hon4@users.noreply.github.com>
2025-04-24 16:49:54 +0300

    CRLF to LF & TAB fixes

--- src/inc/StringSplit.h
+++ src/inc/StringSplit.h
@@ -4,11 +4,11 @@
 #include <any>
 
 std::vector<std::any> StringSplit(const std::string &s, char delimiter) {
-    std::vector<std::any> tokens;
-    std::string token;
-    std::istringstream tokenStream(s);
-    while (std::getline(tokenStream, token, delimiter)) {
-        tokens.push_back(token);
-    }
-    return tokens;
+	std::vector<std::any> tokens;
+	std::string token;
+	std::istringstream tokenStream(s);
+	while (std::getline(tokenStream, token, delimiter)) {
+		tokens.push_back(token);
+	}
+	return tokens;
 }
