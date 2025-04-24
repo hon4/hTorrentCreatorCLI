4a4ce22e3fce63a3a4b9146e3ff15ec26e366435
hon <38807226+hon4@users.noreply.github.com>
2025-04-24 16:54:39 +0300

    CRLF to LF & TAB fixes

--- src/inc/ListFiles.h
+++ src/inc/ListFiles.h
@@ -3,16 +3,16 @@
 #include <vector>
 
 std::vector<std::string> ListFiles(std::string& path) {
-    std::vector<std::string> ret;
-    try {
-        for (const auto& entry : std::filesystem::recursive_directory_iterator(path)) {
-            if (entry.is_regular_file()) {
-                ret.push_back(entry.path().lexically_relative(path).string()); //entry.path().string()
-            }
-        }
-    } catch (const std::filesystem::filesystem_error& err) {
-        std::cerr << "Error: " << err.what() << std::endl;
-    }
+	std::vector<std::string> ret;
+	try {
+		for (const auto& entry : std::filesystem::recursive_directory_iterator(path)) {
+			if (entry.is_regular_file()) {
+				ret.push_back(entry.path().lexically_relative(path).string()); //entry.path().string()
+			}
+		}
+	} catch (const std::filesystem::filesystem_error& err) {
+		std::cerr << "Error: " << err.what() << std::endl;
+	}
 
-    return ret;
+	return ret;
 }
