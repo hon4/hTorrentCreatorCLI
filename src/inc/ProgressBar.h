1af2ea670c3e0da4dd8aa96eb61537309b677b8f
hon <38807226+hon4@users.noreply.github.com>
2025-04-24 16:51:40 +0300

    CRLF to LF & TAB fixes

--- src/inc/ProgressBar.h
+++ src/inc/ProgressBar.h
@@ -2,14 +2,14 @@
 #include <cstdint>
 
 void ShowProgressBar(uint64_t& total, uint64_t& cmpl){
-    int fill = 32 * cmpl / total;
-    std::cout << "\r[";
-    for (int i = 0; i < fill; i++) {
-        std::cout << "=";
-    }
-    for (int i = 0; i < 32 - fill; i++) {
-        std::cout << " ";
-    }
-    int percent = 100 * cmpl / total;
-    std::cout << "] " << percent << "%" << std::flush;
+	int fill = 32 * cmpl / total;
+	std::cout << "\r[";
+	for (int i = 0; i < fill; i++) {
+		std::cout << "=";
+	}
+	for (int i = 0; i < 32 - fill; i++) {
+		std::cout << " ";
+	}
+	int percent = 100 * cmpl / total;
+	std::cout << "] " << percent << "%" << std::flush;
 }
