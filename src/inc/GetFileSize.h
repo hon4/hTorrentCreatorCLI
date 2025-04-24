3bb41d104707f81ce1117fa70846460950400d0a
hon <38807226+hon4@users.noreply.github.com>
2025-04-24 16:58:31 +0300

    CRLF to LF & TAB fixes

--- src/inc/GetFileSize.h
+++ src/inc/GetFileSize.h
@@ -2,9 +2,9 @@
 #include <fstream>
 
 uint64_t GetFileSize(const std::string& filename) {
-    std::ifstream file(filename, std::ios::binary | std::ios::ate);
-    if (!file) {
-        return -1; // Return -1 to indicate an error opening the file
-    }
-    return static_cast<uint64_t>(file.tellg());
+	std::ifstream file(filename, std::ios::binary | std::ios::ate);
+	if (!file) {
+		return -1; // Return -1 to indicate an error opening the file
+	}
+	return static_cast<uint64_t>(file.tellg());
 }
